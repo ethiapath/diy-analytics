add502cec02cb15fa184206a007ac3fdd4cfee0b
Maxime Michel <maxime-michel@users.noreply.github.com>
2020-07-28 09:48:16 +0200

    Read and write file synchronously

--- plugin/index.js
+++ plugin/index.js
@@ -3,15 +3,8 @@ var fs  = require("fs");
 
 module.exports = {
   async onPreBuild({ utils }) {
-
-    fs.readFile(require.resolve('netlify-plugin-diy-analytics/counter.js'), 'utf8', function (err,data) {
-      if (err) {
-        return console.log(err);
-      }
-
-      fs.writeFileSync('counter.js', data);
-    });
-
+    var data = fs.readFileSync(require.resolve('netlify-plugin-diy-analytics/counter.js'), 'utf8');
+    fs.writeFileSync('counter.js', data);
     await utils.functions.add('./counter.js')
   },
 }
