6c377d9182bdceec2d7d9ddc7c677d01ae5134ba
Hargobind Atwal <ethaipath@gmail.com>
2022-05-20 19:22:23 -0700

    add new counter.js path to functions list

--- plugin/netlify-plugin-diy-analytics/index.js
+++ plugin/netlify-plugin-diy-analytics/index.js
@@ -1,10 +1,23 @@
-var netlifyPluginDiyAnalytics = require("netlify-plugin-diy-analytics");
-var fs  = require("fs");
+// var netlifyPluginDiyAnalytics = require("netlify-plugin-diy-analytics");
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   async onPreBuild({ utils }) {
-    var data = fs.readFileSync(require.resolve('netlify-plugin-diy-analytics/counter.js'), 'utf8');
-    fs.writeFileSync('counter.js', data);
-    await utils.functions.add('./counter.js')
+
+    // plugin directory path
+    const dir = './plugin/netlify-plugin-diy-analytics';
+
+    // list all files in the directory
+    fs.readdir(dir, (err, files) => {
+        if (err) { throw err; }
+
+        // files object contains all files names
+        // log them on console
+        files.forEach(file => { console.log(file); });
+    });
+    // var data = fs.readFileSync('./counter.js', 'utf8');
+    // fs.writeFileSync('counter.js', data);
+    await utils.functions.add(path.join(dir, 'counter.js'));
   },
 }
